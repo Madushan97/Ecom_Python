cec04de802952ab83d1dd4fc7330d774cfc75de8
madushan97 <madushannipuna1997@gmail.com>
2021-09-28 19:53:19 +0530

    completed serializers and views of the user

---
+++ ecom/api/user/serializers.py
@@ -0,0 +1,42 @@
+from rest_framwork import serializers
+# this will help us to make our password unreadable
+from django.contrib.auth.hashers import make_password
+from rest_framework.decorators import authentication_classes, permission_classes
+
+from .models import CustomUser
+
+class UserSerializer(serializers.hyperlinkedModelSerializer):
+
+    # create new user
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+    
+    # update user
+
+    def update(self, instance, validated_data):
+        
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
+
+
+
+    class Meta:
+        model = CustomUser
+        # adding extra parameters (add or modify)
+        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('name', 'email', 'password', 'phone', 'gender',
+                 'is_active', 'is_staff', 'is_superuser')
--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.contrib.auth.backends import UserModel
+from rest_framework import viewsets
+from rest_framework.permission import AllowAny
+from .serializers import UserSerializer
+from .models import CustomUser
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, logout
+import re
+
+import random
 
 # Create your views here.
+
+def generate_session_token(length=10):
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)] + [str(i) for i in range(10)]) for _ in range(length))
+
+@csrf_exempt
+def signin(request):
+    if not request.method == 'POST':
+        return JsonResponse({'error': 'Send a post request with valid parameter only'})
+    
+    username = request.POST['email']
+    password = request.POST['password']
+
+    # validation part
+
+    if not re.match("^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", username):
+        return JsonResponse({'error': 'Enter a valid email'})
+
+    if len(password) < 3:
+        return JsonResponse({'error': 'password needs to be at least of 3 characters'})
+
+    UserModel = get_user_model()
+
+    try:
+        user = UserModel.objects.get(email = username)
+
+# matching user's password
+        if user.check_password(password):
+            user_dict = UserModel.objects.filter(email=username).values().first()
+            user_dict.pop('password')
+
+            if user.session_token != "0":
+                user.session_token = "0"
+                user.save()
+                return JsonResponse({'error': 'previous session exist!'})           
+            
+
+            token = generate_session_token()
+            user.session_token = token
+            user.save()
+            login(request, user)
+            return JsonResponse({'token': token, 'user': user_dict})
+
+        else:
+            return JsonResponse({'error': 'Invalid password'})
+
+
+    except UserModel.DoesNotExist:
+        return JsonResponse ({'error': 'Invalid Email'})
