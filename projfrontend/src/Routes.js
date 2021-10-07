c243b9d1cf3cd4831cf640c5ed5d05fe17d5d20e
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2021-10-07 10:48:57 +0530

    sign up form

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,13 +1,17 @@
 import React from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom"
+import PrivateRoutes from "./auth/helper/PrivateRoutes";
 
 import Home from './core/Home'
+import Signup from "./user/Signup";
 
 const Routes = () => {
     return (
         <BrowserRouter>
         <Switch>
             <Route path="/" exact component={Home}/>
+            <Route path="/signup" exact component={Signup}/>
+            {/* <PrivateRoutes path="/user/dashboard" exact component = {}/> */}
         </Switch>
         </BrowserRouter>
     )
